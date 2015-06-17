dd542e4b71c9317cad2a344b7fe5c81726d978cf
Jawad-LiftOff <jawad@liftoffllc.com>
2015-06-17 18:07:32 +0530

    title changed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Prestige' });
+  res.render('index', { title: 'Prestige Shoes' });
 });
 
 module.exports = router;
